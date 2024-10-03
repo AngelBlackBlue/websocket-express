2e5bcfa439ab274558feba2910cc7d4b07786dc2
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-10-03 16:33:34 -0300

    sockt io

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 import express from "express";
 import logger from "morgan";
 
-const port = process.env.PORT ?? 3000;
-const app = express();
-app.use(logger("dev"));
+import { Server } from "socket.io"
+import { createServer } from "node:http"
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected")
+  
+  socket.on("disconnect", () => {
+    console.log("user disconnected")
+  })
 })
 
-app.listen(port, () => {
+
+app.use(logger("dev"));
+
+
+app.get("/", (req, res) => {
+  //process.cwd() desde donde se ejecute
+  res.sendFile(process.cwd() + "/client/index.html")
+})
+
+server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
     
 })
